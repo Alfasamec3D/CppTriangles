4afe16c449827b6cdaf674dea2fe9fea39d6b986
Ivan <navigatorprestigio7777@gmail.com>
2026-03-20 00:52:56 +0300

    fixed some errors

--- include/geom.hpp
+++ include/geom.hpp
@@ -3,8 +3,9 @@
 #include <algorithm>
 #include <array>
 #include <cassert>
+#include <cmath>
+#include <iostream>
 #include <stdexcept>
-#include<iostream>
 namespace Geometry {
 
 const double global_tolerance = 0.000001;
@@ -81,7 +82,7 @@ VPoint<T> operator/(const VPoint<T>& vpoint, const U& number) {
 
 template <typename T>
 VPoint<T> operator-(const VPoint<T>& vpoint) {
-  return (-1) * vpoint;
+  return T{-1} * vpoint;
 }
 
 template <typename T>
@@ -125,14 +126,14 @@ T sqr_distance(const VPoint<T>& point1, const VPoint<T>& point2) {
   return scalar_product(difference_point, difference_point);
 }
 
-/*template <typename T>
+template <typename T>
 std::ostream& operator<<(std::ostream& os, const VPoint<T>& vpoint) {
   os << '(';
   for (const auto& coord : vpoint.get_coordinates()) os << coord << ' ';
   os << ')';
   return os;
 }
-*/
+
 template <typename T>
 class Segment {
  private:
@@ -223,31 +224,240 @@ class Triangle {
 };
 
 template <typename T>
-VPoint<T> intersection(const Segment<T>& segment, const Triangle<T>& triangle);
+VPoint<T> intersection(const Segment<T>& segment, const Triangle<T>& triangle) {
+  return segment.get_points()[0] +
+         segment.direction() *
+             scalar_product(triangle.get_points()[0] - segment.get_points()[0],
+                            triangle.normal()) /
+             scalar_product(segment.direction(), triangle.normal());
+}
 
 template <typename T>
 bool intersect(const VPoint<T>& vpoint, const Segment<T>& segment,
-               const double& tolerance = global_tolerance);
+               const double& tolerance = global_tolerance) {
+  auto scal_prod = scalar_product(vpoint - segment.get_points()[0],
+                                  vpoint - segment.get_points()[1]);
+  return approxEql(std::pow(scal_prod, 2),
+                   sqr_distance(segment.get_points()[0], vpoint) *
+                       sqr_distance(segment.get_points()[1], vpoint),
+                   tolerance) &&
+         (approxSgn(scal_prod, tolerance) <= 0);
+}
 
 template <typename T>
 bool intersect(const Segment<T>& segment1, const Segment<T>& segment2,
-               const double& tolerance = global_tolerance);
+               const double& tolerance = global_tolerance) {
+  const std::array<const Segment<T>*, 2> segments = {&segment1, &segment2};
+  const std::array<VPoint<T>, 2> segm_dirs = {segments[0]->direction(),
+                                              segments[1]->direction()};
+  const VPoint<T> vect_prod_dirs = vector_product(segm_dirs[0], segm_dirs[1]);
+
+  std::array<VPoint<T>, 2> n;
+  std::array<std::array<int, 2>, 2> d;
+
+  if (approxSgn(scalar_product(
+                    segments[1]->get_points()[0] - segments[0]->get_points()[0],
+                    vect_prod_dirs),
+                tolerance) != 0)
+    return false;
+
+  for (int i = 0; i < 2; ++i)
+    n[i] = vector_product(segm_dirs[i], vect_prod_dirs);
+
+  for (int i = 0; i < 2; ++i)
+    for (int j = 0; j < 2; ++j)
+      d[i][j] =
+          approxSgn(scalar_product(n[(i + 1) % 2],
+                                   segments[i]->get_points()[j] -
+                                       segments[(i + 1) % 2]->get_points()[0]),
+                    tolerance);
+
+  return ((d[0][0] != d[0][1]) && (d[1][0] != d[1][1])) ||
+         ((d[0][0] == 0) && (d[0][1] == 0) &&
+          ((intersect(segments[0]->get_points()[0], *segments[1], tolerance)) ||
+           (intersect(segments[0]->get_points()[1], *segments[1], tolerance)) ||
+           (intersect(segments[1]->get_points()[0], *segments[0], tolerance))));
+}
 
 template <typename T>
 bool intersect(const VPoint<T>& vpoint, const Triangle<T>& triangle,
-               const double& tolerance = global_tolerance);
+               const double& tolerance = global_tolerance) {
+  if (approxSgn(
+          scalar_product(vpoint - triangle.get_points()[0], triangle.normal()),
+          tolerance) != 0) {
+    return false;
+  }
+  VPoint<T> triangle_normal = triangle.normal();
+  VPoint<T> expected_normal;
+  VPoint<T> custom_normal;
+  Triangle<T> custom_triangle;
+  for (int i = 0; i < 3; ++i) {
+    custom_triangle = Triangle<T>{
+        {vpoint, triangle.get_points()[i], triangle.get_points()[(i + 1) % 3]}};
+    custom_normal = custom_triangle.normal();
+    expected_normal +=
+        custom_normal *
+        approxSgn(scalar_product(triangle_normal, custom_normal), tolerance);
+  }
+  return approxEql(triangle_normal, expected_normal, tolerance);
+}
 
 template <typename T>
 bool intersect(const Segment<T>& segment, const Triangle<T>& triangle,
-               const double& tolerance = global_tolerance);
+               const double& tolerance = global_tolerance) {
+  VPoint<T> triangle_normal = triangle.normal();
+  int d1 = approxSgn(
+      scalar_product(segment.get_points()[0] - triangle.get_points()[0],
+                     triangle_normal),
+      tolerance);
+  int d2 = approxSgn(
+      scalar_product(segment.get_points()[1] - triangle.get_points()[0],
+                     triangle_normal),
+      tolerance);
+
+  if ((d1 + d2 == 2) || (d1 + d2 == -2)) {
+    return false;
+  } else if ((d1 == 0) && (d2 == 0)) {
+    bool intersect_statement = false;
+    for (int i = 0; i < 3; ++i) {
+      intersect_statement |= intersect(
+          triangle.get_points()[(i + 1) % 3] - triangle.get_points()[i],
+          segment, tolerance);
+    }
+    return intersect(segment.get_points()[0], triangle, tolerance) ||
+           intersect(segment.get_points()[1], triangle, tolerance) ||
+           intersect_statement;
+  } else if ((d1 == 0) && (d2 != 0)) {
+    if (intersect(segment.get_points()[0], triangle, tolerance)) {
+      return true;
+    }
+  } else if ((d1 != 0) && (d2 == 0)) {
+    if (intersect(segment.get_points()[1], triangle, tolerance)) {
+      return true;
+    }
+  }
+  return intersect(intersection(segment, triangle), triangle, tolerance);
+}
 
 template <typename T>
 bool subintersect(const Triangle<T>& triangle1, const Triangle<T>& triangle2,
-                  const double& tolerance = global_tolerance);
+                  const double& tolerance = global_tolerance) {
+  if ((triangle1.actual_class(tolerance) == VPOINT) &&
+      (triangle2.actual_class(tolerance) == VPOINT)) {
+    return approxEql(triangle1.get_points()[0], triangle2.get_points()[0],
+                     tolerance);
+  }
+  if ((triangle1.actual_class(tolerance) == VPOINT) &&
+      (triangle2.actual_class(tolerance) == SEGMENT)) {
+    return intersect(triangle1.get_points()[0], triangle2.segment(), tolerance);
+  }
+  if ((triangle1.actual_class(tolerance) == SEGMENT) &&
+      (triangle2.actual_class(tolerance) == VPOINT)) {
+    return intersect(triangle2.get_points()[0], triangle1.segment(), tolerance);
+  }
+  if ((triangle1.actual_class(tolerance) == SEGMENT) &&
+      (triangle2.actual_class(tolerance) == SEGMENT)) {
+    return intersect(triangle1.segment(), triangle2.segment(), tolerance);
+  }
+  if ((triangle1.actual_class(tolerance) == VPOINT) &&
+      (triangle2.actual_class(tolerance) == TRIANGLE)) {
+    return intersect(triangle1.get_points()[0], triangle2, tolerance);
+  }
+  if ((triangle1.actual_class(tolerance) == TRIANGLE) &&
+      (triangle2.actual_class(tolerance) == VPOINT)) {
+    return intersect(triangle2.get_points()[0], triangle1), tolerance;
+  }
+  if ((triangle1.actual_class(tolerance) == SEGMENT) &&
+      (triangle2.actual_class(tolerance) == TRIANGLE)) {
+    return intersect(triangle1.segment(), triangle2), tolerance;
+  }
+
+  return intersect(triangle2.segment(), triangle1), tolerance;
+}
 
 template <typename T>
 bool intersect(const Triangle<T>& triangle1, const Triangle<T>& triangle2,
-               const double& tolerance = global_tolerance);
+               const double& tolerance = global_tolerance) {
+  if ((triangle1.actual_class() == TRIANGLE) &&
+      (triangle2.actual_class() == TRIANGLE)) {
+    std::array<const Triangle<T>*, 2> triangle = {&triangle1, &triangle2};
+    std::array<std::array<int, 3>, 2> d;
+    for (int j = 0; j < 2; ++j)
+      for (int i = 0; i < 3; ++i) {
+        d[j][i] =
+            approxSgn(scalar_product(triangle[j]->get_points()[i] -
+                                         triangle[(j + 1) % 2]->get_points()[0],
+                                     triangle[(j + 1) % 2]->normal()),
+                      tolerance);
+      }
+    if (((d[0][0] == d[0][1]) && (d[0][1] == d[0][2]) && (d[0][0] != 0)) ||
+        ((d[1][0] == d[1][1]) && (d[1][1] == d[1][2]) && (d[1][0] != 0)))
+      return false;
+    if ((d[0][0] == 0) && (d[0][1] == 0) && (d[0][2] == 0))
+      return intersect(triangle[0]->get_points()[0], *triangle[1], tolerance) ||
+             intersect(triangle[0]->get_points()[1], *triangle[1], tolerance) ||
+             intersect(triangle[0]->get_points()[2], *triangle[1], tolerance) ||
+             intersect(triangle[1]->get_points()[0], *triangle[0], tolerance) ||
+             intersect(triangle[1]->get_points()[1], *triangle[0], tolerance) ||
+             intersect(triangle[1]->get_points()[2], *triangle[0], tolerance);
+    // Having examined these 2 cases, we understand, the polygons intersect each
+    // other's planes, but do not lie on them
+    std::array<Segment<T>, 2> segment;
+    std::array<std::array<VPoint<T>, 2>, 2> t;
+    std::array<int, 2> k;
+    for (int j = 0; j < 2; ++j)
+      for (int i = 0; i < 3; ++i) {
+        // every apex of triangle j doesn't lie on the triangle j+1's plane:
+        if ((d[j][i] == -d[j][(i + 1) % 3]) &&
+            (d[j][(i + 1) % 3] == d[j][(i + 2) % 3])) {
+          k[j] = 2;
+          segment[0] = {{triangle[j]->get_points()[i],
+                         triangle[j]->get_points()[(i + 1) % 3]}};
+          segment[1] = {{triangle[j]->get_points()[(i + 2) % 3],
+                         triangle[j]->get_points()[i]}};
+          t[j][0] = intersection(segment[0], *triangle[(j + 1) % 2]);
+          t[j][1] = intersection(segment[1], *triangle[(j + 1) % 2]);
+          break;
+        }
+        // only 2 apexes of triangle j lie on the plane of triangle j+1:
+        if ((d[j][i] == 0) && (d[j][(i + 1) % 3] == 0)) {
+          k[j] = 2;
+          t[j][0] = triangle[j]->get_points()[i];
+          t[j][1] = triangle[j]->get_points()[(i + 1) % 3];
+          break;
+        }
+        // only 1 apex of triangle j lie on the plane of triangle j+1:
+        if ((d[j][i] == 0) && (d[j][(i + 1) % 3] == -d[j][(i + 2) % 3])) {
+          k[j] = 2;
+          t[j][0] = triangle[j]->get_points()[i];
+          segment[0] = {{triangle[j]->get_points()[(i + 1) % 3],
+                         triangle[j]->get_points()[(i + 2) % 3]}};
+          t[j][1] = intersection(segment[0], *triangle[(j + 1) % 2]);
+          break;
+        }
+
+        if ((d[j][i] == 0) && (d[j][(i + 1) % 3] == d[j][(i + 2) % 3])) {
+          k[j] = 1;
+          t[j][0] = triangle[j]->get_points()[i];
+          break;
+        }
+      }
+    std::array<Segment<T>, 2> final_segments = {Segment<T>{{t[1][0], t[1][1]}},
+                                                Segment<T>{{t[0][0], t[0][1]}}};
+    if ((k[0] == 1) && (k[1] == 1)) {
+      return approxEql(t[0][1], t[0][0], tolerance);
+    } else if ((k[0] == 1) && (k[1] == 2)) {
+      return intersect(t[0][0], final_segments[0], tolerance);
+    } else if ((k[0] == 2) && (k[1] == 1)) {
+      return intersect(t[1][0], final_segments[1], tolerance);
+    } else {
+      return intersect(final_segments[0], final_segments[1], tolerance);
+    }
+  }
+  // if one of the triangles is actually not a triangle
+  return subintersect(triangle1, triangle2, tolerance);
+}
+
 template <typename T>
 struct AABB {
   VPoint<T> minCorner, maxCorner;
